fff7065b6948a9e77a53c4253a44ea66142a2956
XuetongWang-DRR <x.wang-12@student.utwente.nl>
2022-07-28 09:45:53 +0200

    Update preparefinaldata.R

--- preparefinaldata.R
+++ preparefinaldata.R
@@ -27,12 +27,12 @@ for (f in files){
 library(raster)
 
 
-setwd("D:/ITC/thesis_Turkey/R/IMERG/Mar-Jun/Testing")
-GPM1 = read.table("IMERG20190301E.txt",sep = "\t", header = T)
+setwd("D:/ITC/thesis_Turkey/R/IMERG/Mar-Jun/Training")
+GPM1 = read.table("IMERG20150301E.txt",sep = "\t", header = T)
 #GPM1 = rasterFromXYZ(as.data.frame(GPM1)[,c("Lon","Lat","Rainfall")])
 
-setwd("D:/ITC/thesis_Turkey/R/IDW/Mar-Jun/Testing")
-IDW1 = read.table("20190301E.txt",sep = "\t", header = T)
+setwd("D:/ITC/thesis_Turkey/R/IDW/Mar-Jun/Training")
+IDW1 = read.table("20150301E.txt",sep = "\t", header = T)
 #IDW1 = rasterFromXYZ(as.data.frame(IDW1)[,c("Lon","Lat","Rainfall")])
 #par(mfrow = c(1,2))
 #plot(IDW1)
@@ -59,33 +59,38 @@ colnames(GPM) = c("Lon","Lat","Elevation","Eastness","Northness","NDVI",
                    "IDW","OCK")
 
 
-setwd("D:/ITC/thesis_Turkey/R/IMERG/Mar-Jun/Testing")
+setwd("D:/ITC/thesis_Turkey/R/IMERG/Mar-Jun/Training")
 a = list.files()
 #need change according to the situation
-a = a[-c(1,124)]
+a = a[-c(1,124,247)]
+library(lubridate)
 
 for (i in a){
 T = substr(i,6,13)
 D = as_date(T)
 DP = D-1
 DP = as.numeric(format(DP,"%Y%m%d"))
+TP = D-10
+TP = as.numeric(format(TP,"%Y%m%d"))
 
-setwd("D:/ITC/thesis_Turkey/R/IMERG/Mar-Jun/Testing")
+
+setwd("D:/ITC/thesis_Turkey/R/IMERG/Mar-Jun/Training")
 GPM0 = read.table(paste0("IMERG",DP,"E.txt"),sep = "\t", header = TRUE)
 GPM1 = read.table(paste0("IMERG",T,"E.txt"),sep = "\t", header = TRUE)
 
 
-setwd("D:/ITC/thesis_Turkey/R/IDW/Mar-Jun/Testing")
+setwd("D:/ITC/thesis_Turkey/R/IDW/Mar-Jun/Training")
 IDW1 = read.table(paste0(T,"E.txt"),sep = "\t", header = TRUE)
-setwd("D:/ITC/thesis_Turkey/R/OCK/Mar-Jun/Testing")
+setwd("D:/ITC/thesis_Turkey/R/OCK/Mar-Jun/Training")
 OCK1 = read.table(paste0(T,"E.txt"),sep = "\t", header = FALSE)
 colnames(OCK1) = c("Lon","Lat","Rainfall")
 
 GPM1$Elevation = cov$Elevation
 GPM1$Eastness = cov$Eastness
 GPM1$Northness = cov$Northness
-setwd("D:/ITC/thesis_Turkey/R/NDVI/Mar-Jun/Testing")
-NDVI = raster(paste0("NDVI",T,".tif"))
+setwd("D:/ITC/thesis_Turkey/R/NDVI/all")
+NDVI = raster(paste0("NDVI",TP,".tif"))
+
 NDVI = as.data.frame(rasterToPoints(NDVI))
 colnames(NDVI) = c("Lon","Lat","NDVI")
 
@@ -113,11 +118,11 @@ GPM = rbind.data.frame(GPM,GPM1)
 }
 GPM = GPM[-1,]
 
-setwd("D:/ITC/thesis_Turkey/model/new")
-write.table(GPM,"TestJulOct.txt", sep = "\t", col.names = TRUE, row.names = FALSE)
+setwd("D:/ITC/thesis_Turkey/model/newnew")
+write.table(GPM,"TrainMarJun.txt", sep = "\t", col.names = TRUE, row.names = FALSE)
 
-GPM = read.table("ValiJulOct.txt",sep = "\t", header = TRUE)
-GPM$extra = rep(c(1:123),each = 1130)
+GPM = read.table("ValiMarJun.txt",sep = "\t", header = TRUE)
+GPM$extra = rep(c(1:122),each = 1130)
 library(dplyr)
 set.seed(1)
 GPM <- GPM %>% group_by(extra) %>% sample_n(size = 200)
@@ -139,14 +144,14 @@ qrF_model <- quantregForest(x = GPM[,1:46],
                             nodesize = 5,
                             mtry = 5,
                             ntree = 500)
-saveRDS(object = qrF_model,file = "JulOct_IDW_5_5_500_200sample.rds")
+saveRDS(object = qrF_model,file = "NovFeb_IDW_5_5_500_200sample.rds")
 Sys.time()
 
 
 Validation 
 
 
-mod1 = readRDS (file = "NovFeb_IDW_5_5_500_500sample.rds")
+mod1 = readRDS (file = "MarJun_IDW_5_5_500_200sample.rds")
 qrF_model = mod1
 
 qrF_prediction <- predict(qrF_model,
@@ -158,5 +163,76 @@ GPM2 = GPM[,-49]
 GPM2$predict = qrF_prediction
 GPM2$origin = GPM$min1
 library(Metrics)
+GPM2 = na.omit(GPM2)
 rmse(GPM2$IDW,GPM2$origin)
 rmse(GPM2$IDW,GPM2$predict)
+
+
+final = GPM2[,c(1,2,47,49,50)]
+final$extra = rep(c(1:122),each = 1130)
+setwd("D:/ITC/thesis_Turkey/R/IMERG/Mar-Jun/Validation")
+a = list.files()
+a = a[-1]
+
+for (i in 1:122){
+setwd("D:/ITC/thesis_Turkey/model/newnew/outMarJun")
+save = paste0("output",substr(a[i],6,13),".txt")
+df = final[final$extra == i,]
+df = df[,-6]
+write.table(df,save, sep = "\t", col.names = TRUE, row.names = FALSE)
+}
+
+library(ggpubr)
+
+setwd("D:/ITC/thesis_Turkey/model/newnew/outNovFeb")
+par(mfrow = c(1,3))
+output = read.table("output20181120.txt",sep = "\t", header = TRUE)
+output = na.omit(output)
+max = max(max(output$IDW),max(output$predict),max(output$origin))
+output1 = rasterFromXYZ(as.data.frame(output)[,c("Lon","Lat","IDW")])
+output2 = rasterFromXYZ(as.data.frame(output)[,c("Lon","Lat","predict")])
+output3 = rasterFromXYZ(as.data.frame(output)[,c("Lon","Lat","origin")])
+
+
+
+p1 = ggR(output1, geom_raster = TRUE) +
+  scale_fill_gradientn("", limits = c(0,max), colours = c("orange", "yellow", "green",  "sky blue","blue"))+
+  theme_bw()+
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank())+
+  ggtitle("IDW")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+p2 = ggR(output2, geom_raster = TRUE) +
+  scale_fill_gradientn("", limits = c(0,max), colours = c("orange", "yellow", "green",  "sky blue","blue"))+
+  theme_bw()+
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank())+
+  ggtitle("predict")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+p3 = ggR(output3, geom_raster = TRUE) +
+  scale_fill_gradientn("", limits = c(0,max), colours = c("orange", "yellow", "green",  "sky blue","blue"))+
+  theme_bw()+
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank())+
+  ggtitle("IMERG")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+ggarrange(p1,p2,p3,ncol = 3,nrow=1,common.legend = T,legend = "right")
+
+
+
+
